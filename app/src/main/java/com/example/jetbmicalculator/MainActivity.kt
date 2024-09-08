d8e75c2c8167a5a7983a1e5d8b06980779cf7c11
ryo-tom <ryskdevelop@gmail.com>
2024-09-08 17:00:36 +0900

    s3: UIベース作成

--- app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.jetbmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetbmicalculator.ui.theme.JetBMICalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +27,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    Column(
+                        horizontalAlignment = Alignment.Start,
+                        modifier = Modifier.padding(20.dp)
+                    ) {
+                        Text(
+                            text = "BMI計算アプリ",
+                            fontSize = 26.sp,
+                            fontWeight = FontWeight.ExtraBold
+                        )
+                        Spacer(modifier = Modifier.height(30.dp))
+
+                        Text(
+                            text = "身長(cm)",
+                            color = Color(0xFFF85F6A),
+                            fontWeight = FontWeight.Bold
+                        )
+
+                        // TODO: 現時点ではvalueに空文字セットしているだけなため入力しても変化しない
+                        TextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = "",
+                            onValueChange = {}, // TODO: コールバックの設定
+                            colors = TextFieldDefaults.textFieldColors(
+                                backgroundColor = Color.Transparent // 背景色を透明にする
+                            ),
+                            placeholder = { Text(text = "170") },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number), // 数字のみ入力可能なキーボードにする
+                            singleLine = true,
+                        )
+                    }
 
                 }
             }
